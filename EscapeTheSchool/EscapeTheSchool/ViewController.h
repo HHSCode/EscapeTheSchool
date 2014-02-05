07ca295222c8df5cdca73b2cf87e215dcd05e148
Max Greenwald <max@lordtechy.com>
2014-02-05 10:33:22 -0500

    Deleted Kobald Kit Files

--- EscapeTheSchool/EscapeTheSchool/ViewController.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2013 Steffen Itterheim.
- * Released under the MIT License:
- * KoboldAid/licenses/KoboldKitFree.License.txt
- */
-
-@interface ViewController : KKViewController
-
-@end
--- EscapeTheSchool/KoboldKit/KoboldKitExternal/KoboldKitExternal.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2013 Steffen Itterheim.
- * Released under the MIT License:
- * KoboldAid/licenses/KoboldKitFree.License.txt
- */
-
-// external library headers
-#import "KKExternals.h"
