0143c9228ca8f2acb1e3ed842d0bee3d23bdf6ba
Max Greenwald <max@lordtechy.com>
2014-03-12 15:20:24 -0400

    Revert e3675d8..793ed33
    This rolls back to commit e3675d81d39de8e197241cf0dd6af8e9a85764c3.

--- SchoolEscape.spritebuilder/Source/Game.m
+++ SchoolEscape.spritebuilder/Source/Game.m
@@ -7,15 +7,13 @@
 //
 
 #import "Game.h"
-static const CGFloat scrollSpeed = 200.f; //CHANGE SPEED HERE
+static const CGFloat scrollSpeed = 80.f;
 
 @implementation Game{
     CCSprite *_hero;
     CCPhysicsNode *_physicsNode;
     CCNode *_ground1;
     CCNode *_ground2;
-    CCLabelTTF *_coins;
-    CCLabelTTF *_distance;
     NSArray *_grounds;
 }
 
@@ -27,20 +25,10 @@ static const CGFloat scrollSpeed = 200.f; //CHANGE SPEED HERE
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    NSLog(@"Hero: %@", NSStringFromCGPoint(_hero.position));
-    if (_hero.position.y<70) { //Y position is not constant but remains at about 66 when on ground, so this makes the hero only jump or duck when on the ground
-        
-    if (touch.locationInWorld.x<=[[UIScreen mainScreen] bounds].size.height/2) {
-        [_hero.physicsBody applyImpulse:ccp(0, 40.f)];  //JUMP HEIGHT
-
-    }else{ //ADD DUCKING HERE
-        
-    }
-    }
+    [_hero.physicsBody applyImpulse:ccp(0, 40.f)];  //JUMP HEIGHT
 }
 
 - (void)update:(CCTime)delta {
-    [_distance setString:[NSString stringWithFormat:@"%f", _hero.position.x]];
     _hero.position = ccp(_hero.position.x + delta * scrollSpeed, _hero.position.y);
     float yVelocity = clampf(_hero.physicsBody.velocity.y, -1 * MAXFLOAT, 200.f);
     _hero.physicsBody.velocity = ccp(0, yVelocity);
