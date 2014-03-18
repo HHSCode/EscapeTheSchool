879ed444f4847b8b4c06616aa87269c615b0b43d
John Kotz <jkotzlyme@gmail.com>
2014-03-18 10:57:02 -0400

    Gamcenter work

---
+++ School Escape/School Escape/GCHelper.h
@@ -0,0 +1,22 @@
+//
+//  GCHelper.h
+//  School Escape
+//
+//  Created by Sudikoff Lab iMac on 3/18/14.
+//  Copyright (c) 2014 Lordtechy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCHelper : NSObject{
+    // This class is the implementation of the Game Center Helper
+    BOOL gameCenterAvailable;
+    BOOL userAuthenticated;
+}
+
+@property (assign, readonly) BOOL gameCenterAvailable;
+
++ (GCHelper *)sharedInstance;
+- (void)authenticateLocalUser;
+
+@end
---
+++ School Escape/School Escape/GCHelper.m
@@ -0,0 +1,26 @@
+//
+//  GCHelper.m
+//  School Escape
+//
+//  Created by Sudikoff Lab iMac on 3/18/14.
+//  Copyright (c) 2014 Lordtechy. All rights reserved.
+//
+
+#import "GCHelper.h"
+
+@implementation GCHelper
+@synthesize gameCenterAvailable;
+
+-(void)authenticateLocalUser{
+    
+}
+
+# pragma mark Initialization
+
+#warning Game Center is not validated yet!
+
++(GCHelper *)sharedInstance{
+    
+}
+
+@end
